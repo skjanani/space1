f55ffd88b1256cdd5032642303f0d692366d26a0
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-30 10:02:36 +0530

    Add files via upload

---
+++ Space.java
@@ -0,0 +1,19 @@
+package janani;
+import java.util.Scanner;
+public class Space {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+//String str=String.valueOf(n);
+//int l=str.length();
+String []a={"one","two","three","four","five","six","seven","eight","nine","ten"};
+//for(i=0;i<l;i++){
+	System.out.print(a[n-1]);
+}
+	}
+
+
